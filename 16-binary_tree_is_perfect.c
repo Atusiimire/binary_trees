3427803e7d2fe834358011ed8ae47dee4df0f59a
Atusiimire <mutatinamoureen@gmail.com>
2022-11-16 13:21:35 -0800

    first commit

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,58 +1,53 @@
 #include "binary_trees.h"
 
-int findADepth(const binary_tree_t *node);
-int isPerfectRec(const binary_tree_t *root, int d, int level);
-
 /**
- * binary_tree_is_perfect - checks if tree is a perfect tree
- * @tree: tree to check
- * Return: 1 (perfect) 0 (not perfect)
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node:  pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0
  */
 
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if (node && !node->left  && !node->right)
+return (1);
+return (0);
+}
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height of
+ * Return: the height of the tree. If tree is NULL, return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t left, right;
+if (tree == NULL || binary_tree_is_leaf(tree))
+return (0);
+left = binary_tree_height(tree->left);
+right = binary_tree_height(tree->right);
+if (left > right)
+return (1 + left);
+return (1 + right);
+}
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if perfect, 0 otherwise. If tree is NULL, return 0
+ */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-int depth;
-if (!tree)
+binary_tree_t *l, *r;
+if (tree == NULL)
 return (0);
-depth = findADepth(tree);
-return (isPerfectRec(tree, depth, 0));
-}
-/**
- * findADepth - Returns depth of leftmost leaf.
- * @node: root node of tree
- * Return: depth
- */
-int findADepth(const binary_tree_t *node)
-{
-int d = 0;
-while (node != NULL)
-{
-d++;
-node = node->left;
-}
-return (d);
-}
-/**
- * isPerfectRec - recursively checks if a tree is perfect
- * @root: root node of tree
- * @d: depth of the tree
- * @level: current level of node
- * Return: 1 (if perfect) 0 (if not)
- */
-int isPerfectRec(const binary_tree_t *root, int d, int level)
-{
-if (root == NULL)
+l = tree->left;
+r = tree->right;
+if (binary_tree_is_leaf(tree))
+return (1);
+if (l == NULL || r == NULL)
 return (0);
-/**
- * If leaf node, then its depth must be same as
- * depth of all other leaves.
- */
-if (root->left == NULL && root->right == NULL)
-return (d == level + 1);
-/*If internal node and one child is empty*/
-if (root->left == NULL || root->right == NULL)
+if (binary_tree_height(l) == binary_tree_height(r))
+{
+if (binary_tree_is_perfect(l) && binary_tree_is_perfect(r))
+return (1);
+}
 return (0);
-/*Left and right subtrees must be perfect.*/
-return (isPerfectRec(root->left, d, level + 1) &&
-isPerfectRec(root->right, d, level + 1));
 }
